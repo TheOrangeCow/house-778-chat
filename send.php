fd20af6472da0c316d734e032ad233d44f23bb75
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-04-01 14:47:30 +0100

    update

--- send.php
+++ send.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 include "../base/chech.php"; 
 include "../base/main.php";
 session_start(); 
